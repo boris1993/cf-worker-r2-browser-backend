0534aedcf288047d3ec0f142a58963b0323d8942
Boris Zhao <boris1993@live.cn>
2023-11-25 15:14:10 +0800

    Support downloading single file

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export default {
 				const url = new URL(request.url);
 				const pathParts = url.pathname.slice(1).split('/')
 				const bucketName = pathParts[0];
+
 				if (!isBucketAccessAllowed(allowedBuckets, bucketName)) {
 					return new Response(
 						'Bucket doesn\'t exists',
@@ -26,23 +27,12 @@ export default {
 					);
 				}
 
-				const objectName = pathParts[1];
-
-				const r2ListOptions: R2ListOptions = {
-					prefix: url.searchParams.get('prefix') ?? undefined,
-					delimiter: url.searchParams.get('delimiter') ?? undefined,
-					cursor: url.searchParams.get('cursor') ?? undefined,
+				if (pathParts.length === 1) {
+					return listObjectsInBucket(env, bucketName);
 				}
 
-				const listing = await env.DOWNLOAD.list(r2ListOptions);
-				return new Response(
-					JSON.stringify(listing), 
-					{
-						headers: {
-							'content-type': 'application/json; charset=UTF-8',
-						}
-					}
-				);
+				const objectName = pathParts[1];
+				return downloadFileFromBucket(env, bucketName, objectName);
 			default:
 				return new Response('Method not allowed', {
 					status: 405
@@ -53,4 +43,45 @@ export default {
 
 function isBucketAccessAllowed(allowedBuckets: string[], bucketAccessing: string): boolean {
 	return allowedBuckets.includes(bucketAccessing) || allowedBuckets.includes(`${bucketAccessing}-preview`);
+}
+
+async function listObjectsInBucket(env: Env, bucket: string): Promise<Response> {
+	// const r2ListOptions: R2ListOptions = {
+	// 	prefix: url.searchParams.get('prefix') ?? undefined,
+	// 	delimiter: url.searchParams.get('delimiter') ?? undefined,
+	// 	cursor: url.searchParams.get('cursor') ?? undefined,
+	// }
+
+	const listing = await env.DOWNLOAD.list();
+	return new Response(
+		JSON.stringify(listing),
+		{
+			headers: {
+				'content-type': 'application/json; charset=UTF-8',
+			}
+		}
+	);
+}
+
+async function downloadFileFromBucket(env: Env, bucketName: string, filename: string): Promise<Response> {
+	const object = await env.DOWNLOAD.get(filename);
+	if (object == null) {
+		return new Response(
+			'File not found',
+			{
+				status: 404
+			}
+		)
+	}
+
+	const headers = new Headers();
+	object.writeHttpMetadata(headers);
+	headers.set('etag', object.httpEtag);
+
+	return new Response(
+		object.body, 
+		{
+			headers: headers
+		}
+	);
 }
